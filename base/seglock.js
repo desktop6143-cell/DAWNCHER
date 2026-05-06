302de1831cf953c59eade8f9e102d43c189fe604
Kleitom Rian J.D.S <desktop6143@gmail.com>
2026-05-06 14:00:18 -0300

    Update seglock.js

--- base/seglock.js
+++ base/seglock.js
@@ -1,1 +1,6 @@
- if (sessionStorage.getItem('logado') !== 'true') {window.location.href = "../index.html";}
+if (sessionStorage.getItem('logado') !== 'true') {
+    // Verifica se já não estamos na página de login para evitar loop infinito
+    if (!window.location.href.includes("index.html")) {
+        window.location.href = "../index.html"; 
+    }
+}
